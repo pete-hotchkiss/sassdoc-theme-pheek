2f67ae39e62ee79f0ace9d39d9093f266210ad4c
pete hotchkiss <pete@pheekaboo.io>
2016-08-14 16:10:29 +0100

    gulp-sass now being used to build out assets/css file (#1, #2)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -19,23 +19,25 @@ Task: styles
 Initiated as a pre-task to the minify-css task, this takes all the .scss SASS files and creates the sites css file, applying appropriate browserpre-fixes as needed. Assumes that sourcemaps are required, unless the --production flag is passed.
 */
 // var sass = require('gulp-sass');
-// gulp.task('styles', () => {
-//   return gulp.src(['src/sass/main.scss'])
-//     .pipe($.plumber())
-//     .pipe($.sourcemaps.init())
-//     // .pipe($.sass.sync({
-//     //   outputStyle: 'expanded',
-//     //   precision: 10,
-//     //   includePaths: ['.']
-//     // }).on('error', $.sass.logError) )
-//     .pipe($.sass({
-//       outputStyle: 'expanded',
-//       precision: 10,
-//       includePaths: ['.']
-//     }).on('error', $.sass.logError) )
-//     .pipe(gulp.dest('.tmp/css'))
-//     .pipe(reload({stream: true}));
-// });
+gulp.task('styles', () => {
+  return gulp.src(['scss/main.scss'])
+    .pipe( $.print() )
+    .pipe( $.plumber() )
+    .pipe( $.sourcemaps.init() )
+    // .pipe($.sass.sync({
+    //   outputStyle: 'expanded',
+    //   precision: 10,
+    //   includePaths: ['.']
+    // }).on('error', $.sass.logError) )
+    .pipe($.sass({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError) )
+    .pipe( $.stripCssComments() )
+    .pipe(gulp.dest('./assets/css'));
+    // .pipe(reload({stream: true}));
+});
 
 gulp.task('build', ()=> {
   return gulp.src(['assets/js/vendor/*.js', 'assets/js/*.js', '!assets/js/main.min.js'])
